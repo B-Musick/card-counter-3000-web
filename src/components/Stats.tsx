3f9f865ae687cf18537202bc4504a0e9f230fcd0
B-Musick <bmuze1@gmail.com>
2024-09-06 10:39:05 -0500

    #15 - implemented modal showing when list item clicked. Need to add tables now.

--- src/components/Stats.tsx
+++ src/components/Stats.tsx
@@ -1,10 +1,11 @@
-import { ToggleButton, ToggleButtonGroup } from "@mui/material";
+import { Box, Modal, ToggleButton, ToggleButtonGroup } from "@mui/material";
 import { useState } from "react";
 import { StatisticsView } from "../lib/enums";
 import StatsTable from "./StatsTable";
 
 function Stats({data, columns}){
     const [viewShown, setViewShown] = useState<StatisticsView>(StatisticsView.Table);
+    const [modalOpen, setModalOpen] = useState<boolean>(false);
 
     const handleChange = (
         event: React.MouseEvent<HTMLElement>,
@@ -13,19 +14,24 @@ function Stats({data, columns}){
         setViewShown(view);
     };
     
+    let showTable = (row) => {
+        // setSoftTable(useChart(clone(row.results.softs), false, 'stats')[0])
+        // setHardTable(useChart(clone(row.results.hards), true, 'stats')[0])
+        // setSplitTable(useChart(clone(row.results.splits), false, 'stats')[0])
+        setModalOpen(true);
+    }
+    
     let rows = data.map((data, index) => {
         return { ...data, score: `${data.score}/${data.total}`, created_at: new Date(data.created_at).toLocaleDateString(), percent: Number((data.percent).toFixed(2)) }
     })
 
     let statsTable = rows && viewShown == StatisticsView.Table &&
         <StatsTable
-        cols={columns}
-        rows={rows}
-        hiddenCols={['results', 'total', 'id', 'updated_at']}
-        sortable={['created_at', 'percent']} 
-        onRowClick={function (): {} {
-            throw new Error("Function not implemented.");
-        } } rowConditionals={undefined}        />
+            cols={columns}
+            rows={rows}
+            hiddenCols={['results', 'total', 'id', 'updated_at']}
+            sortable={['created_at', 'percent']} 
+            onRowClick={(row)=>showTable(row)} rowConditionals={undefined}        />
     
     return (
         <>
@@ -40,7 +46,19 @@ function Stats({data, columns}){
                 <ToggleButton value={StatisticsView.Table} className="w-1/2">Table</ToggleButton>
                 <ToggleButton value={StatisticsView.Chart} className="w-1/2">Chart</ToggleButton>
             </ToggleButtonGroup>
+
             {statsTable}
+
+            <Modal
+                open={modalOpen}
+                onClose={()=>setModalOpen(false)}
+                aria-labelledby="modal-modal-title"
+                aria-describedby="modal-modal-description"
+            >
+                <Box className="absolute top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%]" >
+                    <div className="bg-white rounded-xl">hi modal</div>
+                </Box>
+            </Modal>
         </>
     )
 }
